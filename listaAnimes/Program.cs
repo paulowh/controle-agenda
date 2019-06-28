60e968cda19556e6e5824e05e3bf8545f9265085
Paulo-Not <ph744915@gmail.com>
2019-06-28 12:48:40 -0300

    Tela de Esquci a senha

--- listaAnimes/Program.cs
+++ listaAnimes/Program.cs
@@ -16,7 +16,7 @@ namespace listaAnimes
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCadastro());
+            Application.Run(new frmEsqueciSenha());
         }
     }
 }
